f6108e49fe9e79184c342c9319912a48b748f6f0
kuldeep.yadav <ykuldeep777@gmail.com>
2025-08-29 18:31:15 +0530

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const { merge } = require("mochawesome-merge");
+const generate = require("mochawesome-report-generator");
 
 module.exports = defineConfig({
   projectId: "j4ci1d",
  reporter: 'cypress-mochawesome-reporter',
  reporterOptions: {
+  reportDir: "cypress/reports/html",
+  overwrite: false,
+  html: true,
+  json: true,
   charts: true,
-  reportPageTitle: 'custom-title',
+  reportPageTitle: 'Cypress E2E test',
   embeddedScreenshots: true,
   inlineAssets: true,
   videoOnFailOnly:true,
@@ -21,6 +27,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
     specPattern: 'cypress/integration/testExamples/*.js',
     video: true,       // <-- Enable video recording
